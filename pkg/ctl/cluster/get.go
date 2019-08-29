b704f6b9feca7dc3fbdee35b9b4b7ede36bcc461
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-08-29 11:11:43 +0800

    Fix descriptions

--- pkg/ctl/cluster/get.go
+++ pkg/ctl/cluster/get.go
@@ -10,7 +10,7 @@ import (
 func getClusterDataCmd(vc *cmdutils.VerbCmd) {
 	desc := pulsar.LongDescription{}
 	desc.CommandUsedFor = "This command is used for getting the cluster data of the specified cluster."
-	desc.CommandPermission = "This command requires super-user permission."
+	desc.CommandPermission = "This command requires super-user permissions."
 
 	var examples []pulsar.Example
 	get := pulsar.Example{
