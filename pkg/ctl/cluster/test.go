9a592fb97836efbdd99e046678a7b9ab3d57957e
xiaolong.ran <ranxiaolong716@gmail.com>
2019-08-29 21:13:26 +0800

    add auth test case
    Signed-off-by: xiaolong.ran <ranxiaolong716@gmail.com>

--- pkg/ctl/cluster/test.go
+++ pkg/ctl/cluster/test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/kris-nova/logger"
 	"github.com/spf13/cobra"
 	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+	"os"
 )
 
 func TestClusterCommands(newVerb func(cmd *cmdutils.VerbCmd), args []string) (out *bytes.Buffer, err error)  {
@@ -33,5 +34,65 @@ func TestClusterCommands(newVerb func(cmd *cmdutils.VerbCmd), args []string) (ou
 	rootCmd.AddCommand(resourceCmd)
 	err = rootCmd.Execute()
 
+	return buf, err
+}
+
+var (
+	flag     bool
+	basePath string
+)
+
+func TestTlsHelp(newVerb func(cmd *cmdutils.VerbCmd), args []string)(out *bytes.Buffer, err error) {
+	var rootCmd = &cobra.Command {
+		Use:	"pulsarctl [command]",
+		Short: 	"a CLI for Apache Pulsar",
+		Run: func(cmd *cobra.Command, _ []string) {
+			if err := cmd.Help(); err != nil {
+				logger.Debug("ignoring error %q", err.Error())
+			}
+		},
+	}
+
+	if !flag {
+		basePath, err = os.Getwd()
+		if err != nil {
+			return nil, err
+		}
+
+		err = os.Chdir("../../../")
+		if err != nil {
+			return nil, err
+		}
+
+		basePath, err = os.Getwd()
+		if err != nil {
+			return nil, err
+		}
+		flag = true
+	}
+
+	baseArgs := []string{
+		"--auth-params",
+		"{\"tlsCertFile\":\""+basePath+"/test/auth/certs/client-cert.pem\""+
+		",\"tlsKeyFile\":\""+basePath+"/test/auth/certs/client-key.pem\"}",
+		"--tls-trust-cert-pat", basePath+"/test/auth/certs/cacert.pem",
+		"--admin-service-url", "https://localhost:8443",
+		"--tls-allow-insecure"}
+
+	buf := new(bytes.Buffer)
+	rootCmd.SetOut(buf)
+	rootCmd.SetArgs(append(baseArgs, args...))
+	resourceCmd := cmdutils.NewResourceCmd(
+		"clusters",
+		"Operations about cluster(s)",
+		"",
+		"cluster")
+
+	flagGrouping := cmdutils.NewGrouping()
+
+	rootCmd.AddCommand(resourceCmd)
+	cmdutils.AddVerbCmd(flagGrouping, resourceCmd, newVerb)
+	rootCmd.PersistentFlags().AddFlagSet(cmdutils.PulsarCtlConfig.FlagSet())
+	err = rootCmd.Execute()
 	return buf, err
 }
---
+++ pkg/ctl/cluster/tls_test.go
@@ -0,0 +1,30 @@
+package cluster
+
+import (
+    `github.com/stretchr/testify/assert`
+    `strings`
+    `testing`
+)
+
+func TestTLS(t *testing.T) {
+    args := []string{"clusters","add", "tls"}
+    _, err := TestTlsHelp(createClusterCmd, args)
+    assert.Nil(t, err)
+
+    args = []string{"clusters","list"}
+    out, err := TestTlsHelp(listClustersCmd, args)
+    assert.Nil(t, err)
+    clusters := out.String()
+    assert.True(t, strings.Contains(clusters, "tls"))
+
+    args = []string{"clusters","delete", "tls"}
+    _, err = TestTlsHelp(deleteClusterCmd, args)
+    assert.Nil(t, err)
+
+    args = []string{"clusters","list"}
+    out, err = TestTlsHelp(listClustersCmd, args)
+    assert.Nil(t, err)
+    clusters = out.String()
+    assert.False(t, strings.Contains(clusters, "tls"))
+}
+
