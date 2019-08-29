ed3f70931163ec72f0bd61347fd5a1829fd031e6
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-08-29 17:31:49 +0800

    Add command cluster `get-peer-clusters` ---
    Master Issue: #2

--- pkg/pulsar/cluster.go
+++ pkg/pulsar/cluster.go
@@ -51,14 +51,14 @@ func (c *clusters) Update(cdata ClusterData) error {
 	endpoint := c.client.endpoint(c.basePath, cdata.Name)
 	return c.client.post(endpoint, &cdata, nil)
 }
-
-func (c *clusters) UpdatePeerClusters(cluster string, peerClusters []string) error {
-	endpoint := c.client.endpoint(c.basePath, cluster, "peers")
-	return c.client.post(endpoint, peerClusters, nil)
-}
 func (c *clusters) GetPeerClusters(name string) ([]string, error) {
 	var peerClusters []string
 	endpoint := c.client.endpoint(c.basePath, name, "peers")
 	err := c.client.get(endpoint, &peerClusters)
 	return peerClusters, err
+}
+
+func (c *clusters) UpdatePeerClusters(cluster string, peerClusters []string) error {
+	endpoint := c.client.endpoint(c.basePath, cluster, "peers")
+	return c.client.post(endpoint, peerClusters, nil)
 }
