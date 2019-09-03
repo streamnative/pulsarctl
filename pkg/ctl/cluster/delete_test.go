064bb44a78c1e0e0b7efb43f8a970bbfca95d9ee
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-03 16:50:29 +0800

    Add handler to handle test error (#27)
    ### Motivation
    
    We can't get the error info when running a command. When testing a command, we need to verify the error output.
    
    ### Modification
    
    Make a handler to handle errors. Create a test handler in TestClusterCommand for test other commands.

--- pkg/ctl/cluster/delete_test.go
+++ pkg/ctl/cluster/delete_test.go
@@ -8,21 +8,21 @@ import (
 
 func TestDeleteClusterCmd(t *testing.T) {
 	args := []string{"add", "test"}
-	_, err := TestClusterCommands(createClusterCmd, args)
+	_, _, _, err := TestClusterCommands(createClusterCmd, args)
 	assert.Nil(t, err)
 
 	args = []string{"list"}
-	out, err := TestClusterCommands(listClustersCmd, args)
+	out, _, _, err := TestClusterCommands(listClustersCmd, args)
 	assert.Nil(t, err)
 	clusters := out.String()
 	assert.True(t, strings.Contains(clusters, "test"))
 
 	args = []string{"delete", "test"}
-	_, err = TestClusterCommands(deleteClusterCmd, args)
+	_, _, _, err = TestClusterCommands(deleteClusterCmd, args)
 	assert.Nil(t, err)
 
 	args = []string{"list"}
-	out, err = TestClusterCommands(listClustersCmd, args)
+	out, _, _, err = TestClusterCommands(listClustersCmd, args)
 	assert.Nil(t, err)
 	clusters = out.String()
 	assert.False(t, strings.Contains(clusters, "test"))
