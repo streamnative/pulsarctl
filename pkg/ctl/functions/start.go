a7334c742f8258e1663f60a501e9f3dadd3a6f89
冉小龙 <ranxiaolong716@gmail.com>
2019-09-04 14:13:07 +0800

    Add start cmd for Pulsar Functions (#29)
    Master Issue: #2 
    
    Add start cmd for Pulsar Functions
    
    ```
    USED FOR:
        This command is used for starting a stopped function instance.
    
    REQUIRED PERMISSION:
        This command requires super-user permissions.
    
    EXAMPLES:
        #Starts a stopped function instance
        pulsarctl functions start
    	--tenant public
    	--namespace default
    	--name <the name of Pulsar Function>
    
        #Starts a stopped function instance with instance ID
        pulsarctl functions start
    	--tenant public
    	--namespace default
    	--name <the name of Pulsar Function>
    	--instance-id 1
    
        #Starts a stopped function instance with FQFN
        pulsarctl functions start
    	--fqfn tenant/namespace/name [eg: public/default/ExampleFunctions]
    
    OUTPUT:
        #normal output
        Started successfully
    
        #You must specify a name for the Pulsar Functions or a FQFN, please check the --name args
        [✖]  you must specify a name for the function or a Fully Qualified Function Name (FQFN)
    
        #The name of Pulsar Functions doesn't exist, please check the --name args
        [✖]  code: 404 reason: Function <your function name> doesn't exist
    
        #Used an instanceID that does not exist or other impermissible actions
        [✖]  code: 400 reason: Operation not permitted
    
    Usage: pulsarctl functions start [flags]
    
    Aliases: start, start
    
    FunctionsConfig flags:
          --fqfn string          The Fully Qualified Function Name (FQFN) for the function
          --tenant string        The tenant of a Pulsar Function
          --namespace string     The namespace of a Pulsar Function
          --name string          The name of a Pulsar Function
          --instance-id string   The function instanceId (start all instances if instance-id is not provided)
    ```

---
+++ pkg/ctl/functions/start.go
@@ -0,0 +1,161 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package functions
+
+import (
+	"github.com/spf13/pflag"
+	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+	"github.com/streamnative/pulsarctl/pkg/pulsar"
+	"strconv"
+)
+
+func startFunctionsCmd(vc *cmdutils.VerbCmd) {
+	desc := pulsar.LongDescription{}
+	desc.CommandUsedFor = "This command is used for starting a stopped function instance."
+	desc.CommandPermission = "This command requires super-user permissions."
+
+	var examples []pulsar.Example
+
+	start := pulsar.Example{
+		Desc: "Starts a stopped function instance",
+		Command: "pulsarctl functions start \n" +
+			"\t--tenant public\n" +
+			"\t--namespace default\n" +
+			"\t--name <the name of Pulsar Function>",
+	}
+	examples = append(examples, start)
+
+	startWithInstanceID := pulsar.Example{
+		Desc: "Starts a stopped function instance with instance ID",
+		Command: "pulsarctl functions start \n" +
+			"\t--tenant public\n" +
+			"\t--namespace default\n" +
+			"\t--name <the name of Pulsar Function>\n" +
+			"\t--instance-id 1",
+	}
+	examples = append(examples, startWithInstanceID)
+
+	startWithFQFN := pulsar.Example{
+		Desc: "Starts a stopped function instance with FQFN",
+		Command: "pulsarctl functions start \n" +
+			"\t--fqfn tenant/namespace/name [eg: public/default/ExampleFunctions]",
+	}
+	examples = append(examples, startWithFQFN)
+	desc.CommandExamples = examples
+
+	var out []pulsar.Output
+	successOut := pulsar.Output{
+		Desc: "normal output",
+		Out:  "Started successfully",
+	}
+
+	failOut := pulsar.Output{
+		Desc: "You must specify a name for the Pulsar Functions or a FQFN, please check the --name args",
+		Out:  "[✖]  you must specify a name for the function or a Fully Qualified Function Name (FQFN)",
+	}
+
+	failOutWithNameNotExist := pulsar.Output{
+		Desc: "The name of Pulsar Functions doesn't exist, please check the --name args",
+		Out:  "[✖]  code: 404 reason: Function <your function name> doesn't exist",
+	}
+
+	failOutWithWrongInstanceID := pulsar.Output{
+		Desc: "Used an instanceID that does not exist or other impermissible actions",
+		Out:  "[✖]  code: 400 reason: Operation not permitted",
+	}
+
+	out = append(out, successOut, failOut, failOutWithNameNotExist, failOutWithWrongInstanceID)
+	desc.CommandOutput = out
+
+	vc.SetDescription(
+		"start",
+		"Starts a stopped function instance",
+		desc.ToString(),
+		"start",
+	)
+
+	functionData := &pulsar.FunctionData{}
+
+	// set the run function
+	vc.SetRunFunc(func() error {
+		return doStartFunctions(vc, functionData)
+	})
+
+	// register the params
+	vc.FlagSetGroup.InFlagSet("FunctionsConfig", func(flagSet *pflag.FlagSet) {
+		flagSet.StringVar(
+			&functionData.FQFN,
+			"fqfn",
+			"",
+			"The Fully Qualified Function Name (FQFN) for the function")
+
+		flagSet.StringVar(
+			&functionData.Tenant,
+			"tenant",
+			"",
+			"The tenant of a Pulsar Function")
+
+		flagSet.StringVar(
+			&functionData.Namespace,
+			"namespace",
+			"",
+			"The namespace of a Pulsar Function")
+
+		flagSet.StringVar(
+			&functionData.FuncName,
+			"name",
+			"",
+			"The name of a Pulsar Function")
+
+		flagSet.StringVar(
+			&functionData.InstanceID,
+			"instance-id",
+			"",
+			"The function instanceId (start all instances if instance-id is not provided)")
+	})
+}
+
+func doStartFunctions(vc *cmdutils.VerbCmd, funcData *pulsar.FunctionData) error {
+	err := processBaseArguments(funcData)
+	if err != nil {
+		vc.Command.Help()
+		return err
+	}
+
+	admin := cmdutils.NewPulsarClientWithApiVersion(pulsar.V3)
+	if funcData.InstanceID != "" {
+		instanceID, err := strconv.Atoi(funcData.InstanceID)
+		if err != nil {
+			return err
+		}
+		err = admin.Functions().StartFunctionWithID(funcData.Tenant, funcData.Namespace, funcData.FuncName, instanceID)
+		if err != nil {
+			return err
+		}
+		vc.Command.Printf("Started instanceID[%s] of Pulsar Functions[%s] successfully ", funcData.InstanceID, funcData.FuncName)
+	} else {
+		err = admin.Functions().StartFunction(funcData.Tenant, funcData.Namespace, funcData.FuncName)
+		if err != nil {
+			return err
+		}
+
+		vc.Command.Printf("Started %s successfully", funcData.FuncName)
+	}
+
+	return nil
+}
---
+++ pkg/ctl/functions/start_test.go
@@ -0,0 +1,129 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package functions
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestStartFunctions(t *testing.T) {
+	jarName := "dummyExample.jar"
+	_, err := os.Create(jarName)
+	assert.Nil(t, err)
+
+	defer os.Remove(jarName)
+	args := []string{"create",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-start",
+		"--inputs", "test-input-topic",
+		"--output", "persistent://public/default/test-output-topic",
+		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
+		"--jar", jarName,
+	}
+
+	_, _, err = TestFunctionsCommands(createFunctionsCmd, args)
+	assert.Nil(t, err)
+
+	stopArgs := []string{"stop",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-start",
+	}
+
+	_, _, err = TestFunctionsCommands(stopFunctionsCmd, stopArgs)
+	assert.Nil(t, err)
+
+	startArgs := []string{"start",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-start",
+	}
+
+	_, _, err = TestFunctionsCommands(startFunctionsCmd, startArgs)
+	assert.Nil(t, err)
+
+	argsFqfn := []string{"create",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-start-fqfn",
+		"--inputs", "test-input-topic",
+		"--output", "persistent://public/default/test-output-topic",
+		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
+		"--jar", jarName,
+	}
+
+	_, _, err = TestFunctionsCommands(createFunctionsCmd, argsFqfn)
+	assert.Nil(t, err)
+
+	stopArgsFqfn := []string{"stop",
+		"--fqfn", "public/default/test-functions-start-fqfn",
+	}
+
+	_, _, err = TestFunctionsCommands(stopFunctionsCmd, stopArgsFqfn)
+	assert.Nil(t, err)
+
+	startArgsFqfn := []string{"start",
+		"--fqfn", "public/default/test-functions-start-fqfn",
+	}
+
+	_, _, err = TestFunctionsCommands(startFunctionsCmd, startArgsFqfn)
+	assert.Nil(t, err)
+
+	// test failure cases
+
+	stopArgsFqfnAgain := []string{"stop",
+		"--fqfn", "public/default/test-functions-start-fqfn",
+	}
+	_, _, err = TestFunctionsCommands(stopFunctionsCmd, stopArgsFqfnAgain)
+	assert.Nil(t, err)
+
+	// test the function name not exist
+	failureStartArgs := []string{"start",
+		"--name", "not-exist",
+	}
+	_, err, _ = TestFunctionsCommands(startFunctionsCmd, failureStartArgs)
+	assert.NotNil(t, err)
+	failMsg := "Function not-exist doesn't exist"
+	assert.True(t, strings.ContainsAny(err.Error(), failMsg))
+
+	// test the --name args not exist
+	notExistNameOrFqfnArgs := []string{"start",
+		"--tenant", "public",
+		"--namespace", "default",
+	}
+	_, err, _ = TestFunctionsCommands(startFunctionsCmd, notExistNameOrFqfnArgs)
+	assert.NotNil(t, err)
+	failNameMsg := "you must specify a name for the function or a Fully Qualified Function Name (FQFN)"
+	assert.True(t, strings.ContainsAny(err.Error(), failNameMsg))
+
+	// test the instance id not exist
+	notExistInstanceIDArgs := []string{"start",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-start-fqfn",
+		"--instance-id", "12345678",
+	}
+	_, err, _ = TestFunctionsCommands(startFunctionsCmd, notExistInstanceIDArgs)
+	assert.NotNil(t, err)
+	failInstanceIDMsg := "Operation not permitted"
+	assert.True(t, strings.ContainsAny(err.Error(), failInstanceIDMsg))
+}
