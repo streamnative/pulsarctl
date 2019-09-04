c52f0906e2ce089792c693bf5033bbc622c7493f
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-04 14:14:00 +0800

    Add command cluster `list-failure-domains` (#23)
    Master issue: #2 
    
    ```
    ➜  pulsarctl git:(clusters_list_failure_domain) ./pulsarctl cluster list-failure-domains -h
    USED FOR:
        This command is used for getting all failure domain under the cluster <cluster-name>.
    
    REQUIRED PERMISSION:
        This command requires super-user permissions.
    
    EXAMPLES:
        #listing all the failure domains under the specified cluster
        pulsarctl clusters list-failure-domains <cluster-name>
    
    OUTPUT:
        #output example
        {
          "failure-domain": {
            "brokers": [
              "failure-broker-A",
              "failure-broker-B"
            ]
          }
        }
    
        #the cluster name is not specified or the cluster name is specified more than one
        [✖]  only one argument is allowed to be used as a name
    
        #the specified cluster does not exist in the broker
        [✖]  code: 404 reason: Cluster does not exist
    
    Usage: pulsarctl clusters list-failure-domains [flags]
    
    Aliases: list-failure-domains, lfd
    
    Common flags:
      -s, --admin-service-url string    The admin web service url that pulsarctl connects to. (default "http://localhost:8080")
          --auth-params string          Authentication parameters are used to configure the public and private key files required by tls
                                         For example: "tlsCertFile:val1,tlsKeyFile:val2"
      -C, --color string                toggle colorized logs (true,false,fabulous) (default "true")
      -h, --help                        help for this command
          --tls-allow-insecure          Allow TLS insecure connection
          --tls-trust-cert-pat string   Allow TLS trust cert file path
      -v, --verbose int                 set log level, use 0 to silence, 4 for debugging (default 3)
    
    Use 'pulsarctl clusters list-failure-domains [command] --help' for more information about a command.
    ```

---
+++ pkg/ctl/cluster/list_failure_domain_test.go
@@ -0,0 +1,52 @@
+package cluster
+
+import (
+	"encoding/json"
+	"github.com/streamnative/pulsarctl/pkg/pulsar"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestListFailureDomainsCmd(t *testing.T)  {
+	args := []string{"create", "list-failure-test"}
+	_, _, _, err := TestClusterCommands(createClusterCmd, args)
+	assert.Nil(t, err)
+
+	args = []string{"create", "list-failure-broker-A"}
+	_, _, _, err = TestClusterCommands(createClusterCmd, args)
+	assert.Nil(t, err)
+
+	args = []string{"create", "list-failure-broker-B"}
+	_, _, _, err = TestClusterCommands(createClusterCmd, args)
+	assert.Nil(t, err)
+
+	args = []string{"create-failure-domain", "--broker-list", "list-failure-broker-A", "list-failure-test", "list-failure-A"}
+	_, _, _, err = TestClusterCommands(createFailureDomainCmd, args)
+	assert.Nil(t, err)
+
+	args = []string{"create-failure-domain", "--broker-list", "list-failure-broker-B", "list-failure-test", "list-failure-B"}
+	_, _, _, err = TestClusterCommands(createFailureDomainCmd, args)
+	assert.Nil(t, err)
+
+	args = []string{"list-failure-domains", "list-failure-test"}
+	out, _, _, err := TestClusterCommands(listFailureDomainCmd, args)
+	assert.Nil(t, err)
+
+	var brokerMap pulsar.FailureDomainMap
+	err = json.Unmarshal(out.Bytes(), &brokerMap)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, brokerMap["list-failure-A"])
+	assert.Equal(t, "list-failure-broker-A", brokerMap["list-failure-A"].BrokerList[0])
+	assert.NotNil(t, brokerMap["list-failure-B"])
+	assert.Equal(t, "list-failure-broker-B", brokerMap["list-failure-B"].BrokerList[0])
+}
+
+func TestListFailureArgsError(t *testing.T)  {
+	args := []string{"list-failure-domains"}
+	_, _, nameErr, _ := TestClusterCommands(listFailureDomainCmd, args)
+	assert.NotNil(t, nameErr)
+	assert.Equal(t, "only one argument is allowed to be used as a name", nameErr.Error())
+}
