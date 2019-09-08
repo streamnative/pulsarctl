bf7db2ab2cc6b34bbc97e5124cb3c295d1d1962e
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-08 23:13:42 +0800

    address comment

--- pkg/ctl/tenant/list.go
+++ pkg/ctl/tenant/list.go
@@ -44,11 +44,6 @@ func listTenantCmd(vc *cmdutils.VerbCmd) {
 }
 
 func doListTenant(vc *cmdutils.VerbCmd) error {
-	// for testing
-	if vc.NameError != nil {
-		return vc.NameError
-	}
-
 	admin := cmdutils.NewPulsarClient()
 	tenants, err := admin.Tenants().List()
 	if err == nil {
