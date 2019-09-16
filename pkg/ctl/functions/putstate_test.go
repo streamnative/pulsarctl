31801baa3ac6c337e26211c982b621205fdab1e9
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-16 19:49:53 +0800

    Remove code comments

--- pkg/ctl/functions/putstate_test.go
+++ pkg/ctl/functions/putstate_test.go
@@ -20,176 +20,176 @@
 
 package functions
 
-//func TestStateFunctions(t *testing.T) {
-//	basePath, err := getDirHelp()
-//	if basePath == "" || err != nil {
-//		t.Error(err)
-//	}
-//	t.Logf("base path: %s", basePath)
-//	args := []string{"create",
-//		"--tenant", "public",
-//		"--namespace", "default",
-//		"--name", "test-functions-putstate",
-//		"--inputs", "test-input-topic",
-//		"--output", "persistent://public/default/test-output-topic",
-//		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
-//		"--jar", basePath + "/test/functions/api-examples.jar",
-//	}
-//
-//	out, execErr, err := TestFunctionsCommands(createFunctionsCmd, args)
-//	assert.Nil(t, err)
-//	if execErr != nil {
-//		t.Errorf("create functions error value: %s", execErr.Error())
-//	}
-//	assert.Equal(t, out.String(), "Created test-functions-putstate successfully")
-//
-//	putstateArgs := []string{"putstate",
-//		"--tenant", "public",
-//		"--namespace", "default",
-//		"--name", "test-functions-putstate",
-//		"pulsar", "-", "hello",
-//	}
-//
-//	outPutState := new(bytes.Buffer)
-//
-//	for {
-//		outPutState, _, _ = TestFunctionsCommands(putstateFunctionsCmd, putstateArgs)
-//
-//		if strings.Contains(outPutState.String(), "successfully") {
-//			break
-//		}
-//	}
-//
-//	assert.True(t, strings.Contains(outPutState.String(), "successfully"))
-//
-//	// test failure case for put state
-//	failureStateArgs := []string{"putstate",
-//		"--name", "not-exist",
-//		"pulsar", "-", "hello",
-//	}
-//
-//	stateArgsErrInFormat := []string{"putstate",
-//		"--name", "test-functions-putstate",
-//		"pulsar", "hello",
-//	}
-//
-//	_, execErrMsg, _ := TestFunctionsCommands(putstateFunctionsCmd, failureStateArgs)
-//	assert.NotNil(t, execErrMsg)
-//	exceptMsg := "'not-exist' is not found"
-//	t.Logf("error message:%s", execErrMsg.Error())
-//	assert.True(t, strings.Contains(execErrMsg.Error(), exceptMsg))
-//
-//	_, errMsg, _ := TestFunctionsCommands(putstateFunctionsCmd, stateArgsErrInFormat)
-//	assert.NotNil(t, errMsg)
-//	exceptErrMsg := "error input format"
-//	t.Logf("err message:%s", errMsg.Error())
-//	assert.True(t, strings.Contains(errMsg.Error(), exceptErrMsg))
-//
-//	// query state
-//	queryStateArgs := []string{"querystate",
-//		"--tenant", "public",
-//		"--namespace", "default",
-//		"--name", "test-functions-putstate",
-//		"--key", "pulsar",
-//	}
-//
-//	outQueryState, _, err := TestFunctionsCommands(querystateFunctionsCmd, queryStateArgs)
-//	assert.Nil(t, err)
-//	t.Logf("outQueryState:%s", outQueryState.String())
-//
-//	var state pulsar.FunctionState
-//	err = json.Unmarshal(outQueryState.Bytes(), &state)
-//	assert.Nil(t, err)
-//
-//	assert.Equal(t, "pulsar", state.Key)
-//	assert.Equal(t, "hello", state.StringValue)
-//	assert.Equal(t, int64(0), state.Version)
-//	// put state again
-//	outPutStateAgain, _, err := TestFunctionsCommands(putstateFunctionsCmd, putstateArgs)
-//	assert.Nil(t, err)
-//	assert.True(t, strings.Contains(outPutStateAgain.String(), "successfully"))
-//
-//	// query state again
-//	outQueryStateAgain, _, err := TestFunctionsCommands(querystateFunctionsCmd, queryStateArgs)
-//	assert.Nil(t, err)
-//
-//	var stateAgain pulsar.FunctionState
-//	err = json.Unmarshal(outQueryStateAgain.Bytes(), &stateAgain)
-//	assert.Nil(t, err)
-//
-//	assert.Equal(t, int64(1), stateAgain.Version)
-//}
-//
-//func TestByteValue(t *testing.T) {
-//	basePath, err := getDirHelp()
-//	if basePath == "" || err != nil {
-//		t.Error(err)
-//	}
-//	t.Logf("base path: %s", basePath)
-//	args := []string{"create",
-//		"--tenant", "public",
-//		"--namespace", "default",
-//		"--name", "test-functions-putstate-byte-value",
-//		"--inputs", "test-input-topic",
-//		"--output", "persistent://public/default/test-output-topic",
-//		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
-//		"--jar", basePath + "/test/functions/api-examples.jar",
-//	}
-//
-//	out, execErr, err := TestFunctionsCommands(createFunctionsCmd, args)
-//	assert.Nil(t, err)
-//	if execErr != nil {
-//		t.Errorf("create functions error value: %s", execErr.Error())
-//	}
-//	assert.Equal(t, out.String(), "Created test-functions-putstate-byte-value successfully")
-//
-//	buf := "hello pulsar!"
-//	file, err := ioutil.TempFile("", "tmpfile")
-//	if err != nil {
-//		panic(err)
-//	}
-//	defer os.Remove(file.Name())
-//	if _, err := file.Write([]byte(buf)); err != nil {
-//		panic(err)
-//	}
-//
-//	t.Logf("file name:%s", file.Name())
-//
-//	putstateArgs := []string{"putstate",
-//		"--tenant", "public",
-//		"--namespace", "default",
-//		"--name", "test-functions-putstate-byte-value",
-//		"pulsar", "=", file.Name(),
-//	}
-//
-//	outPutState := new(bytes.Buffer)
-//
-//	for {
-//		outPutState, _, _ = TestFunctionsCommands(putstateFunctionsCmd, putstateArgs)
-//
-//		if strings.Contains(outPutState.String(), "successfully") {
-//			break
-//		}
-//	}
-//
-//	assert.True(t, strings.Contains(outPutState.String(), "successfully"))
-//
-//	// query state
-//	queryStateArgs := []string{"querystate",
-//		"--tenant", "public",
-//		"--namespace", "default",
-//		"--name", "test-functions-putstate-byte-value",
-//		"--key", "pulsar",
-//	}
-//
-//	outQueryState, _, err := TestFunctionsCommands(querystateFunctionsCmd, queryStateArgs)
-//	assert.Nil(t, err)
-//	t.Logf("outQueryState:%s", outQueryState.String())
-//
-//	var state pulsar.FunctionState
-//	err = json.Unmarshal(outQueryState.Bytes(), &state)
-//	assert.Nil(t, err)
-//
-//	assert.Equal(t, "pulsar", state.Key)
-//	assert.Equal(t, "hello pulsar!", state.StringValue)
-//}
+func TestStateFunctions(t *testing.T) {
+	basePath, err := getDirHelp()
+	if basePath == "" || err != nil {
+		t.Error(err)
+	}
+	t.Logf("base path: %s", basePath)
+	args := []string{"create",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-putstate",
+		"--inputs", "test-input-topic",
+		"--output", "persistent://public/default/test-output-topic",
+		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
+		"--jar", basePath + "/test/functions/api-examples.jar",
+	}
+
+	out, execErr, err := TestFunctionsCommands(createFunctionsCmd, args)
+	assert.Nil(t, err)
+	if execErr != nil {
+		t.Errorf("create functions error value: %s", execErr.Error())
+	}
+	assert.Equal(t, out.String(), "Created test-functions-putstate successfully")
+
+	putstateArgs := []string{"putstate",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-putstate",
+		"pulsar", "-", "hello",
+	}
+
+	outPutState := new(bytes.Buffer)
+
+	for {
+		outPutState, _, _ = TestFunctionsCommands(putstateFunctionsCmd, putstateArgs)
+
+		if strings.Contains(outPutState.String(), "successfully") {
+			break
+		}
+	}
+
+	assert.True(t, strings.Contains(outPutState.String(), "successfully"))
+
+	// test failure case for put state
+	failureStateArgs := []string{"putstate",
+		"--name", "not-exist",
+		"pulsar", "-", "hello",
+	}
+
+	stateArgsErrInFormat := []string{"putstate",
+		"--name", "test-functions-putstate",
+		"pulsar", "hello",
+	}
+
+	_, execErrMsg, _ := TestFunctionsCommands(putstateFunctionsCmd, failureStateArgs)
+	assert.NotNil(t, execErrMsg)
+	exceptMsg := "'not-exist' is not found"
+	t.Logf("error message:%s", execErrMsg.Error())
+	assert.True(t, strings.Contains(execErrMsg.Error(), exceptMsg))
+
+	_, errMsg, _ := TestFunctionsCommands(putstateFunctionsCmd, stateArgsErrInFormat)
+	assert.NotNil(t, errMsg)
+	exceptErrMsg := "error input format"
+	t.Logf("err message:%s", errMsg.Error())
+	assert.True(t, strings.Contains(errMsg.Error(), exceptErrMsg))
+
+	// query state
+	queryStateArgs := []string{"querystate",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-putstate",
+		"--key", "pulsar",
+	}
+
+	outQueryState, _, err := TestFunctionsCommands(querystateFunctionsCmd, queryStateArgs)
+	assert.Nil(t, err)
+	t.Logf("outQueryState:%s", outQueryState.String())
+
+	var state pulsar.FunctionState
+	err = json.Unmarshal(outQueryState.Bytes(), &state)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "pulsar", state.Key)
+	assert.Equal(t, "hello", state.StringValue)
+	assert.Equal(t, int64(0), state.Version)
+	// put state again
+	outPutStateAgain, _, err := TestFunctionsCommands(putstateFunctionsCmd, putstateArgs)
+	assert.Nil(t, err)
+	assert.True(t, strings.Contains(outPutStateAgain.String(), "successfully"))
+
+	// query state again
+	outQueryStateAgain, _, err := TestFunctionsCommands(querystateFunctionsCmd, queryStateArgs)
+	assert.Nil(t, err)
+
+	var stateAgain pulsar.FunctionState
+	err = json.Unmarshal(outQueryStateAgain.Bytes(), &stateAgain)
+	assert.Nil(t, err)
+
+	assert.Equal(t, int64(1), stateAgain.Version)
+}
+
+func TestByteValue(t *testing.T) {
+	basePath, err := getDirHelp()
+	if basePath == "" || err != nil {
+		t.Error(err)
+	}
+	t.Logf("base path: %s", basePath)
+	args := []string{"create",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-putstate-byte-value",
+		"--inputs", "test-input-topic",
+		"--output", "persistent://public/default/test-output-topic",
+		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
+		"--jar", basePath + "/test/functions/api-examples.jar",
+	}
+
+	out, execErr, err := TestFunctionsCommands(createFunctionsCmd, args)
+	assert.Nil(t, err)
+	if execErr != nil {
+		t.Errorf("create functions error value: %s", execErr.Error())
+	}
+	assert.Equal(t, out.String(), "Created test-functions-putstate-byte-value successfully")
+
+	buf := "hello pulsar!"
+	file, err := ioutil.TempFile("", "tmpfile")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(file.Name())
+	if _, err := file.Write([]byte(buf)); err != nil {
+		panic(err)
+	}
+
+	t.Logf("file name:%s", file.Name())
+
+	putstateArgs := []string{"putstate",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-putstate-byte-value",
+		"pulsar", "=", file.Name(),
+	}
+
+	outPutState := new(bytes.Buffer)
+
+	for {
+		outPutState, _, _ = TestFunctionsCommands(putstateFunctionsCmd, putstateArgs)
+
+		if strings.Contains(outPutState.String(), "successfully") {
+			break
+		}
+	}
+
+	assert.True(t, strings.Contains(outPutState.String(), "successfully"))
+
+	// query state
+	queryStateArgs := []string{"querystate",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-putstate-byte-value",
+		"--key", "pulsar",
+	}
+
+	outQueryState, _, err := TestFunctionsCommands(querystateFunctionsCmd, queryStateArgs)
+	assert.Nil(t, err)
+	t.Logf("outQueryState:%s", outQueryState.String())
+
+	var state pulsar.FunctionState
+	err = json.Unmarshal(outQueryState.Bytes(), &state)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "pulsar", state.Key)
+	assert.Equal(t, "hello pulsar!", state.StringValue)
+}
