6a0a5800dd7d3fcf2479570ab59efecac9c744b7
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-16 19:38:10 +0800

    Add comments in code for re-enbale

--- pkg/ctl/sources/status_test.go
+++ pkg/ctl/sources/status_test.go
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// TODO re-enable the test: https://github.com/streamnative/pulsarctl/issues/60
 // +build functions
 
 package sources
