bf1d54c07f0579c46da6a9424b30e191916ac5dd
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-11-28 22:25:48 +0800

    Support create secret key command ---
    *Motivation*
    
    Support create secret key command. Now can use
    `pulsarctl token create-secret-key` to generate
    a secret key.
    
    *Modifications*
    
    - Add command `create-secret-key`
    - Add test for the command

---
+++ pkg/tokenutil/create_secret_key_test.go
@@ -0,0 +1,108 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package tokenutil
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testData = []struct {
+	InvalidAlgorithm   bool
+	encode             bool
+	SignatureAlgorithm string
+	outputFile         string
+}{
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS256", encode: false, outputFile: ""},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS256", encode: true, outputFile: ""},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS256", encode: false, outputFile: "test-HS256-secret.key"},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS384", encode: false, outputFile: ""},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS384", encode: true, outputFile: ""},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS384", encode: false, outputFile: "test-HS384-secret.key"},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS512", encode: false, outputFile: ""},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS512", encode: true, outputFile: ""},
+	{InvalidAlgorithm: false, SignatureAlgorithm: "HS512", encode: false, outputFile: "test-HS512-secret.key"},
+	{InvalidAlgorithm: true, SignatureAlgorithm: "INVALID", encode: false, outputFile: ""},
+}
+
+func TestCreateSecretKeyCommand(t *testing.T) {
+	for _, data := range testData {
+		if data.InvalidAlgorithm {
+			testInvalidError(t, data.SignatureAlgorithm)
+			continue
+		}
+		log.Printf("test case: %+v", data)
+		testNormalCase(t, data.SignatureAlgorithm, data.outputFile, data.encode)
+	}
+}
+
+func testNormalCase(t *testing.T, signatureAlgorithm, outputFile string, encode bool) {
+	args := []string{"create-secret-key", "--signature-algorithm", signatureAlgorithm}
+	if encode {
+		args = append(args, "--base64")
+	}
+	if outputFile != "" {
+		args = append(args, "--output-file", outputFile)
+		defer os.Remove(outputFile)
+	}
+
+	out, execErr, _ := testTokenCommands(createSecretKey, args)
+	assert.Nil(t, execErr)
+
+	if encode {
+		assert.Nil(t, execErr)
+		return
+	}
+
+	if outputFile != "" {
+		assert.Nil(t, execErr)
+		assert.Equal(t,
+			fmt.Sprintf("Write the secret key to the file %s successfully.\n", outputFile),
+			out.String())
+		return
+	}
+
+	switch {
+	case signatureAlgorithm == "HS256":
+		result := strings.Split(out.String(), " ")
+		assert.Equal(t, 32, len(result))
+	case signatureAlgorithm == "HS384":
+		result := strings.Split(out.String(), " ")
+		assert.Equal(t, 48, len(result))
+	case signatureAlgorithm == "HS512":
+		result := strings.Split(out.String(), " ")
+		assert.Equal(t, 64, len(result))
+	default:
+		log.Fatal("Test error")
+	}
+}
+
+func testInvalidError(t *testing.T, signatureAlgorithm string) {
+	args := []string{"create-secret-key", "--signature-algorithm", signatureAlgorithm}
+	_, execErr, _ := testTokenCommands(createSecretKey, args)
+	assert.NotNil(t, execErr)
+	assert.Equal(t,
+		fmt.Sprintf("the signature algorithm '%s' is invalid. Valid options include: 'HS256', "+
+			"'HS384', 'HS512'\n", signatureAlgorithm),
+		execErr.Error())
+}
