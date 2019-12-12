4849e42a3f85756e31d937a71a8e88ffa0efbb4d
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-12-12 16:21:39 +0800

    Address comments

---
+++ test/script/check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Waiting bookie service start up, if the bookie service does not
+# start up in 20 seconds, that means the bookie service is not start
+# up successfully.
+function checkBookie() {
+    failed=0
+    until curl localhost:8080; do
+        echo waiting service start...
+        sleep 1
+        failed=`expr ${failed} + 1`
+        if [[ ${failed} == 20 ]]; then
+            echo service start up was failed
+            exit 1
+        fi
+    done
+}
+
+case $1 in
+    bookie) checkBookie
+    ;;
+    *) echo Which service you would like to check?
+       echo Available service: bookie
+esac
