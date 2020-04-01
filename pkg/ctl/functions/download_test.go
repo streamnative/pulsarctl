9a8bb78c6234dd5c90ae53586eff02bcc87c2d78
冉小龙 <rxl@apache.org>
2020-04-02 07:09:35 +0800

    Add download command for functions CLI (#221)
    Signed-off-by: xiaolong.ran <rxl@apache.org>

---
+++ pkg/ctl/functions/download_test.go
@@ -0,0 +1,69 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package functions
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDownloadFunctions(t *testing.T) {
+	jarName := "dummyExample.jar"
+	_, err := os.Create(jarName)
+	assert.Nil(t, err)
+
+	defer os.Remove(jarName)
+
+	args := []string{"create",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-download",
+		"--inputs", "test-input-topic",
+		"--output", "persistent://public/default/test-output-topic",
+		"--classname", "org.apache.pulsar.functions.api.examples.ExclamationFunction",
+		"--jar", jarName,
+	}
+
+	_, _, err = TestFunctionsCommands(createFunctionsCmd, args)
+	assert.Nil(t, err)
+
+	downloadArgs := []string{"download",
+		"--tenant", "public",
+		"--namespace", "default",
+		"--name", "test-functions-download",
+		"--destination-file", "./dummyExample.jar",
+	}
+	outPut, execErr, err := TestFunctionsCommands(downloadFunctionsCmd, downloadArgs)
+	assert.Equal(t, outPut.String(), "Downloaded ./dummyExample.jar successfully\n")
+	assert.Nil(t, execErr)
+	assert.Nil(t, err)
+}
+
+func TestDownloadFunctionsWithFailure(t *testing.T) {
+	notExistNameOrFqfnArgs := []string{"download",
+		"--tenant", "public",
+		"--namespace", "default",
+	}
+	_, execErrMsg, _ := TestFunctionsCommands(downloadFunctionsCmd, notExistNameOrFqfnArgs)
+	failMsg := "you must specify a name for the function or a Fully Qualified Function Name (FQFN)"
+	assert.NotNil(t, execErrMsg)
+	assert.True(t, strings.Contains(execErrMsg.Error(), failMsg))
+}
