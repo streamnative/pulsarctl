257db8678b68800e0185512668ebd92a99983908
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-11-24 11:27:53 +0800

    Update the ut tests flow

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly PULSARCTL_HOME=${PULSARCTL_HOME:-"/pulsarctl"}
+pushd ${PULSARCTL_HOME}
+# startup pulsar service
+scripts/pulsar-service-startup.sh
+# run tests
+ go test -v $(go list ./... | grep -v bookkeeper | grep -v bkctl | grep -v functions | grep -v sources | grep -v sinks | grep -v context | grep -v algorithm | grep -v test)
+# stop pulsar service
+scripts/pulsar-service-shutdown.sh
+popd
---
+++ scripts/pulsar-service-startup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+readonly PULSAR_HOME=${PULSAR_HOME:-"/pulsar"}
+
+echo "--- Run pulsar service at the directory ${PULSAR_HOME} ---"
+pushd ${PULSAR_HOME}
+bin/pulsar-daemon start standalone -nss -nfw
+until curl http://localhost:8080/admin/v2/tenants > /dev/null 2>&1
+do
+    sleep 1
+    echo "Wait for pulsar service to be ready...$(date +%H:%M:%S)"
+done
+echo "--- Pulsar service is ready ---"
+popd
