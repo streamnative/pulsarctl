05d3974e759f57a32845520561acde593e5dc88f
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-12-28 17:24:52 +0800

    Update the remaining tests

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -3,7 +3,8 @@ set -e
 
 readonly PROJECT_ROOT=`cd $(dirname $0)/..; pwd`
 readonly IMAGE_NAME=pulsarctl-test
-readonly PULSAR_VERSION=${PULSAR_VERSION:-2.7.0.1}
+readonly PULSAR_DEFAULT_VERSION="2.7.0.1"
+readonly PULSAR_VERSION=${PULSAR_VERSION:-PULSAR_DEFAULT_VERSION}
 
 docker build --build-arg PULSAR_VERSION=${PULSAR_VERSION} \
              -t ${IMAGE_NAME} \
