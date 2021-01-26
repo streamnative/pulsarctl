fd7aadf300bce27d8a6bebbdf96cf1b33fbb9481
Yong Zhang <zhangyong1025.zy@gmail.com>
2021-01-26 10:55:23 +0800

    Fix the tests

--- pkg/ctl/functions/upload_test.go
+++ pkg/ctl/functions/upload_test.go
@@ -22,6 +22,7 @@ import (
 	"encoding/hex"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 	"testing"
 
@@ -48,8 +49,53 @@ func TestUploadWithEmptyArguments(t *testing.T) {
 	assert.True(t, strings.Contains(execErr.Error(), "empty"))
 }
 
+const fileContent = `
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+BINDIR=$(dirname "$0")
+export PULSAR_HOME=$(cd -P $BINDIR/..;pwd)
+. "$PULSAR_HOME/bin/pulsar-admin-common.sh"
+
+#Change to PULSAR_HOME to support relative paths
+cd "$PULSAR_HOME"
+exec $JAVA $OPTS org.apache.pulsar.admin.cli.PulsarAdminTool $PULSAR_CLIENT_CONF "$@"
+`
+
 func TestUploadAndDownloadCommands(t *testing.T) {
-	testFile := "example.go"
+	f, err := ioutil.TempFile(".", "test")
+	defer os.RemoveAll(f.Name())
+	if err != nil {
+		log.Fatal(err)
+		t.Fail()
+		return
+	}
+
+	err = ioutil.WriteFile(f.Name(), []byte(fileContent), os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+		t.Fail()
+		return
+	}
+
+	testFile := f.Name()
 	fileHash, err := getFileSha256(testFile)
 	if err != nil {
 		log.Fatal(err)
@@ -74,6 +120,7 @@ func TestUploadAndDownloadCommands(t *testing.T) {
 		"--path", downloadFilePath,
 	}
 	out, execErr, err = TestFunctionsCommands(downloadFunctionsCmd, args)
+	defer os.RemoveAll(downloadFilePath)
 	assert.Nil(t, err)
 	assert.Nil(t, execErr)
 	assert.True(t, strings.Contains(out.String(), "successfully"))
