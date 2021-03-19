5969f1c081adc66db6a0fb665d3d04e95e4b78b0
Yong Zhang <zhangyong1025.zy@gmail.com>
2021-03-19 15:05:51 +0800

    Fix the error introduce from the latest pulsar (#284)
    * Fix the error introduce from the latest pulsar
    ---
    
    * Update test image

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 readonly PROJECT_ROOT=`cd $(dirname $0)/..; pwd`
 readonly IMAGE_NAME=pulsarctl-test
-readonly PULSAR_DEFAULT_VERSION="2.7.0.5"
+readonly PULSAR_DEFAULT_VERSION="2.7.1.0"
 readonly PULSAR_VERSION=${PULSAR_VERSION:-${PULSAR_DEFAULT_VERSION}}
 
 docker build --build-arg PULSAR_VERSION=${PULSAR_VERSION} \
