fc77b87d60f54d17ef150b3af917b139c3ff49c2
xiaolongran <xiaolongran@tencent.com>
2021-05-27 20:33:26 +0800

    Add get retention cmd for topic
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

---
+++ pkg/ctl/topic/get_retention.go
@@ -0,0 +1,98 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package topic
+
+import (
+	"github.com/spf13/pflag"
+	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+	util "github.com/streamnative/pulsarctl/pkg/pulsar/utils"
+)
+
+func GetRetentionCmd(vc *cmdutils.VerbCmd) {
+	desc := cmdutils.LongDescription{}
+	desc.CommandUsedFor = "Get the retention policy of a topic"
+	desc.CommandPermission = "This command requires tenant admin permissions."
+
+	var examples []cmdutils.Example
+	getRetention := cmdutils.Example{
+		Desc:    "Get the retention policy of a topic",
+		Command: "pulsarctl topics get-retention tenant/namespace/topic",
+	}
+	examples = append(examples, getRetention)
+	desc.CommandExamples = examples
+
+	var out []cmdutils.Output
+	successOut := cmdutils.Output{
+		Desc: "normal output",
+		Out: "{\n" +
+			"  \"RetentionTimeInMinutes\": 0,\n" +
+			"  \"RetentionSizeInMB\": 0\n" +
+			"}",
+	}
+
+	noTopicName := cmdutils.Output{
+		Desc: "you must specify a tenant/namespace/topic name, please check if the tenant/namespace/topic name is provided",
+		Out:  "[✖]  the topic name is not specified or the topic name is specified more than one",
+	}
+
+	tenantNotExistError := cmdutils.Output{
+		Desc: "the tenant does not exist",
+		Out:  "[✖]  code: 404 reason: Tenant does not exist",
+	}
+
+	nsNotExistError := cmdutils.Output{
+		Desc: "the namespace does not exist",
+		Out:  "[✖]  code: 404 reason: Namespace (tenant/namespace) does not exist",
+	}
+
+	out = append(out, successOut, noTopicName, tenantNotExistError, nsNotExistError)
+	desc.CommandOutput = out
+
+	vc.SetDescription(
+		"get-retention",
+		"Get the retention policy of a topic",
+		desc.ToString(),
+		desc.ExampleToString(),
+		"get-retention",
+	)
+
+	var applied bool
+
+	vc.SetRunFuncWithNameArg(func() error {
+		return doGetRetention(vc)
+	}, "the topic name is not specified or the topic name is specified more than one")
+
+	vc.FlagSetGroup.InFlagSet("GrantPermissions", func(set *pflag.FlagSet) {
+		set.BoolVarP(&applied, "applied", "a", false,
+			"Get the applied policy of the topic")
+	})
+}
+
+func doGetRetention(vc *cmdutils.VerbCmd) error {
+	topic := vc.NameArg
+	admin := cmdutils.NewPulsarClient()
+	topicName, err := util.GetTopicName(topic)
+	if err != nil {
+		return err
+	}
+	policy, err := admin.Topics().GetRetention(*topicName)
+	if err == nil {
+		cmdutils.PrintJSON(vc.Command.OutOrStdout(), &policy)
+	}
+	return err
+}
