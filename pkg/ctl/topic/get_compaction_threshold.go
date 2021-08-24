d7e09fb3d06be122cc92fb6163aabffe4e7ef44d
Zixuan Liu <nodeces@gmail.com>
2021-08-24 16:22:23 +0800

    feat: add compaction threshold command for topic
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

---
+++ pkg/ctl/topic/get_compaction_threshold.go
@@ -0,0 +1,74 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package topic
+
+import (
+	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+
+	"github.com/streamnative/pulsarctl/pkg/pulsar/utils"
+)
+
+func GetCompactionThresholdCmd(vc *cmdutils.VerbCmd) {
+	var desc cmdutils.LongDescription
+	desc.CommandUsedFor = "Get the compaction threshold for a topic"
+	desc.CommandPermission = "This command requires tenant admin permissions."
+
+	var examples []cmdutils.Example
+	set := cmdutils.Example{
+		Desc:    "Get the compaction threshold for a topic",
+		Command: "pulsarctl topics get-compaction-threshold [topic]",
+	}
+	examples = append(examples, set)
+	desc.CommandExamples = examples
+
+	var out []cmdutils.Output
+	successOut := cmdutils.Output{
+		Desc: "normal output",
+		Out:  "The compaction threshold of the topic (topic-name) is (size) byte(s)",
+	}
+	out = append(out, successOut)
+	desc.CommandOutput = out
+
+	vc.SetDescription(
+		"get-compaction-threshold",
+		desc.CommandUsedFor,
+		desc.ToString(),
+		desc.ExampleToString())
+
+	var applied bool
+	vc.Command.Flags().BoolVarP(&applied, "applied", "", false, "Get the applied policy for the topic")
+
+	vc.SetRunFuncWithNameArg(func() error {
+		return doGetCompactionThreshold(vc, applied)
+	}, "the topic name is not specified or the topic name is specified more than one")
+}
+
+func doGetCompactionThreshold(vc *cmdutils.VerbCmd, applied bool) error {
+	topic, err := utils.GetTopicName(vc.NameArg)
+	if err != nil {
+		return err
+	}
+
+	admin := cmdutils.NewPulsarClient()
+	threshold, err := admin.Topics().GetCompactionThreshold(*topic, applied)
+	if err == nil {
+		vc.Command.Printf("The compaction threshold of the topic %s is %d byte(s)", topic, threshold)
+	}
+
+	return err
+}
---
+++ pkg/ctl/topic/set_compaction_threshold.go
@@ -0,0 +1,83 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package topic
+
+import (
+	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+
+	utils "github.com/streamnative/pulsarctl/pkg/ctl/utils"
+	util "github.com/streamnative/pulsarctl/pkg/pulsar/utils"
+)
+
+func SetCompactionThresholdCmd(vc *cmdutils.VerbCmd) {
+	var desc cmdutils.LongDescription
+	desc.CommandUsedFor = "Set the compaction threshold for a topic"
+	desc.CommandPermission = "This command requires tenant admin permissions."
+
+	var examples []cmdutils.Example
+	set := cmdutils.Example{
+		Desc:    "Set the compaction threshold for a topic",
+		Command: "pulsarctl topics set-compaction-threshold topic --threshold 3T",
+	}
+	examples = append(examples, set)
+	desc.CommandExamples = examples
+
+	var out []cmdutils.Output
+	successOut := cmdutils.Output{
+		Desc: "normal output",
+		Out:  "Successfully set compaction threshold to (size) for topic (topic-name)",
+	}
+	out = append(out, successOut)
+	desc.CommandOutput = out
+
+	vc.SetDescription(
+		"set-compaction-threshold",
+		desc.CommandUsedFor,
+		desc.ToString(),
+		desc.ExampleToString())
+
+	var threshold string
+	vc.Command.Flags().StringVarP(&threshold, "threshold", "t", "0",
+		"Maximum number of bytes in a topic backlog before compaction is triggered (eg: 10M, 16G, 3T). "+
+			"0 disables automatic compaction")
+	_ = vc.Command.MarkFlagRequired("threshold")
+
+	vc.SetRunFuncWithNameArg(func() error {
+		return doSetCompactionThreshold(vc, threshold)
+	}, "the topic name is not specified or the topic name is specified more than one")
+}
+
+func doSetCompactionThreshold(vc *cmdutils.VerbCmd, threshold string) error {
+	topic, err := util.GetTopicName(vc.NameArg)
+	if err != nil {
+		return err
+	}
+
+	size, err := utils.ValidateSizeString(threshold)
+	if err != nil {
+		return err
+	}
+
+	admin := cmdutils.NewPulsarClient()
+	err = admin.Topics().SetCompactionThreshold(*topic, size)
+	if err == nil {
+		vc.Command.Printf("Successfully set compaction threshold to %d for topic %s", size, topic)
+	}
+
+	return err
+}
