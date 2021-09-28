accaf51e0483d07d84f05040abd180a0111ae44b
Zixuan Liu <nodeces@gmail.com>
2021-09-28 15:56:34 +0800

    feat: add inactive topic policies command for topic (#444)
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>
    
    ### Changes
    
    background from #246,  the PR implements the following commands:
    
    - `pulsarctl topics get-inactive-topic-policies <topic> --applied <bool>`
    - `pulsarctl topics remove-inactive-topic-policies <topic>`
    - `pulsarctl topics set-inactive-topic-policies <topic> --enable-delete-while-inactive <bool> --max-inactive-duration <string> --delete-mode <delete_when_no_subscriptions|delete_when_subscriptions_caught_up>`
    
    (cherry picked from commit db4dda44826d59c566f3d0b748103e732aa0f717)

---
+++ pkg/ctl/topic/inactive_topic_test.go
@@ -0,0 +1,78 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package topic
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/streamnative/pulsarctl/pkg/pulsar/utils"
+	"github.com/streamnative/pulsarctl/pkg/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInactiveTopicCmd(t *testing.T) {
+	topicName := fmt.Sprintf("persistent://public/default/test-inactive-topic-%s",
+		test.RandomSuffix())
+	createArgs := []string{"create", topicName, "1"}
+	_, execErr, _, _ := TestTopicCommands(CreateTopicCmd, createArgs)
+	assert.Nil(t, execErr)
+
+	<-time.After(5 * time.Second)
+
+	setArgs := []string{"set-inactive-topic-policies", topicName,
+		"-e=true",
+		"-t", "1h",
+		"-m", "delete_when_no_subscriptions"}
+	out, execErr, _, _ := TestTopicCommands(SetInactiveTopicCmd, setArgs)
+	assert.Nil(t, execErr)
+	assert.Equal(t, out.String(),
+		fmt.Sprintf("Set inactive topic policies successfully for [%s]", topicName))
+
+	<-time.After(5 * time.Second)
+
+	getArgs := []string{"get-inactive-topic-policies", topicName}
+	out, execErr, _, _ = TestTopicCommands(GetInactiveTopicCmd, getArgs)
+	assert.Nil(t, execErr)
+
+	var inactiveTopic utils.InactiveTopicPolicies
+	err := json.Unmarshal(out.Bytes(), &inactiveTopic)
+	assert.NoError(t, err)
+	assert.Equal(t, true, inactiveTopic.DeleteWhileInactive)
+	assert.Equal(t, 3600, inactiveTopic.MaxInactiveDurationSeconds)
+	assert.Equal(t, "delete_when_no_subscriptions", inactiveTopic.InactiveTopicDeleteMode.String())
+
+	removeArgs := []string{"remove-inactive-topic-policies", topicName}
+	out, execErr, _, _ = TestTopicCommands(RemoveInactiveTopicCmd, removeArgs)
+	assert.Nil(t, execErr)
+	assert.Equal(t, out.String(),
+		fmt.Sprintf("Remove inactive topic policies successfully from [%s]", topicName))
+
+	<-time.After(5 * time.Second)
+
+	out, execErr, _, _ = TestTopicCommands(GetInactiveTopicCmd, getArgs)
+	assert.Nil(t, execErr)
+
+	err = json.Unmarshal(out.Bytes(), &inactiveTopic)
+	assert.NoError(t, err)
+	assert.Equal(t, false, inactiveTopic.DeleteWhileInactive)
+	assert.Equal(t, 0, inactiveTopic.MaxInactiveDurationSeconds)
+	assert.Nil(t, inactiveTopic.InactiveTopicDeleteMode)
+}
