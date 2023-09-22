010ca9cfbed75afb93466b26b862534618078a8b
Guangning E <guangning@streamnative.io>
2023-09-22 15:15:18 +0800

    Support status check for pulsarctl command (#1200) (#1202)
    * Support status check for pulsarctl command (#1200)
    
    * Support read config from env
    * Add site toc content

---
+++ pkg/ctl/status/check.go
@@ -0,0 +1,87 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package status
+
+import (
+	"net/http"
+
+	"github.com/streamnative/pulsarctl/pkg/auth"
+	"github.com/streamnative/pulsarctl/pkg/cli"
+	"github.com/streamnative/pulsarctl/pkg/pulsar"
+	"github.com/streamnative/pulsarctl/pkg/pulsar/common"
+
+	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+)
+
+func checkStatusCmd(vc *cmdutils.VerbCmd) {
+	var desc cmdutils.LongDescription
+	desc.CommandUsedFor = "Check service(broker or proxy) status"
+	desc.CommandPermission = "This command requires super-user permissions."
+
+	var examples []cmdutils.Example
+	check := cmdutils.Example{
+		Desc:    "Check service(broker or proxy) status",
+		Command: "pulsarctl status check",
+	}
+	examples = append(examples, check)
+	desc.CommandExamples = examples
+
+	var out []cmdutils.Output
+	successOut := cmdutils.Output{
+		Desc: "normal output",
+		Out:  "Ok",
+	}
+	out = append(out, successOut)
+	desc.CommandOutput = out
+
+	vc.SetDescription(
+		"check",
+		"Check service(broker or proxy) status",
+		desc.ToString(),
+		desc.ExampleToString(),
+		"check")
+
+	vc.SetRunFunc(func() error {
+		return doCheckStatus(vc)
+	})
+	vc.EnableOutputFlagSet()
+}
+func doCheckStatus(vc *cmdutils.VerbCmd) error {
+	cfg := cmdutils.PulsarCtlConfig
+	if len(cfg.WebServiceURL) == 0 {
+		cfg.WebServiceURL = pulsar.DefaultWebServiceURL
+	}
+	authProvider, err := auth.GetAuthProvider((*common.Config)(cfg))
+	if err != nil {
+		return err
+	}
+	client := &cli.Client{
+		ServiceURL:  cmdutils.PulsarCtlConfig.WebServiceURL,
+		VersionInfo: pulsar.ReleaseVersion,
+		HTTPClient: &http.Client{
+			Timeout:   pulsar.DefaultHTTPTimeOutDuration,
+			Transport: authProvider,
+		},
+	}
+	data, err := client.GetWithQueryParams("/status.html", nil, nil, false)
+	if err != nil {
+		return err
+	}
+	vc.Command.Printf(string(data))
+	return nil
+}
