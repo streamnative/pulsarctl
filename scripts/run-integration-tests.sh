ce224ff6975508c2ba11ba29be471917735d15b8
streamnativebot <streamnativebot@streamnative.io>
2024-02-18 12:46:28 +0000

    "Created by snbot"

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 readonly PROJECT_ROOT=`cd $(dirname $0)/..; pwd`
 readonly IMAGE_NAME=pulsarctl-test
-readonly PULSAR_DEFAULT_VERSION="2.10.5.13"
+readonly PULSAR_DEFAULT_VERSION="2.10.5.13-5ce22c"
 readonly PULSAR_VERSION=${PULSAR_VERSION:-${PULSAR_DEFAULT_VERSION}}
 
 docker build --build-arg PULSAR_VERSION=${PULSAR_VERSION} \
