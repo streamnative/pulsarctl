b9ee88faf268fc149e2cb7161ce5ca7c5208ebfb
Neng Lu <nlu@streamnative.io>
2026-02-18 17:14:57 -0800

    Add namespace-level inactive topic policies commands (#2004)

---
+++ pkg/ctl/namespace/remove_inactive_topic.go
@@ -0,0 +1,65 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package namespace
+
+import (
+	"github.com/apache/pulsar-client-go/pulsaradmin/pkg/utils"
+
+	"github.com/streamnative/pulsarctl/pkg/cmdutils"
+)
+
+func RemoveInactiveTopicCmd(vc *cmdutils.VerbCmd) {
+	var desc cmdutils.LongDescription
+	desc.CommandUsedFor = "Remove inactive topic policies from a namespace"
+	desc.CommandPermission = "This command requires tenant admin permissions."
+
+	var examples []cmdutils.Example
+	examples = append(examples, cmdutils.Example{
+		Desc:    desc.CommandUsedFor,
+		Command: "pulsarctl namespaces remove-inactive-topic-policies tenant/namespace",
+	})
+	desc.CommandExamples = examples
+
+	var out []cmdutils.Output
+	out = append(out, NsErrors...)
+	desc.CommandOutput = out
+
+	vc.SetDescription(
+		"remove-inactive-topic-policies",
+		desc.CommandUsedFor,
+		desc.ToString(),
+		desc.ExampleToString())
+
+	vc.SetRunFuncWithNameArg(func() error {
+		return doRemoveInactiveTopicPolicies(vc)
+	}, "the namespace name is not specified or the namespace name is specified more than one")
+}
+
+func doRemoveInactiveTopicPolicies(vc *cmdutils.VerbCmd) error {
+	ns, err := utils.GetNamespaceName(vc.NameArg)
+	if err != nil {
+		return err
+	}
+
+	admin := cmdutils.NewPulsarClient()
+	err = admin.Namespaces().RemoveInactiveTopicPolicies(*ns)
+	if err == nil {
+		vc.Command.Printf("Remove inactive topic policies successfully from [%s]", ns.String())
+	}
+	return err
+}
